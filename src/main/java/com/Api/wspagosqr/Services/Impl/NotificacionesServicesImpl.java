42af4a4aae072785c80113512c675c236c7c07af
gasfer <gasfer.seca@gmail.com>
2024-03-20 00:49:14 -0400

    configuracion db sql server  yml

--- src/main/java/com/Api/wspagosqr/Services/Impl/NotificacionesServicesImpl.java
+++ src/main/java/com/Api/wspagosqr/Services/Impl/NotificacionesServicesImpl.java
@@ -6,7 +6,11 @@ import com.Api.wspagosqr.Services.NotificacionesServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
 import java.util.function.Predicate;
 
 @Service
@@ -53,4 +57,38 @@ public class NotificacionesServicesImpl implements NotificacionesServices {
             return "hubo un error";
         }
     }
+
+  /*  public  boolean consultaPago(){
+        try {
+            // Realizar la petición
+            URL url = new URL("https://api.frankfurter.app/latest");
+            HttpURLConnection conexion = (HttpURLConnection)url.openConnection();
+            conexion.setRequestMethod("GET");
+            conexion.connect();
+
+            // Comprobar si la respuesta es correcta
+            int respuesta = conexion.getResponseCode();
+            if (respuesta != 200) {
+                // Si no es correcta, mostramos mensaje de error
+                System.out.println("Ha ocurrido el error " + respuesta + " procesando la petición.");
+                return;
+            }
+
+            // Si es correcta, mostramos la respuesta
+            StringBuilder respuestaStr = new StringBuilder();
+            Scanner scanner = new Scanner(url.openStream());
+            while (scanner.hasNext()) {
+                respuestaStr.append(scanner.nextLine());
+            }
+            scanner.close();
+            System.out.println("Respuesta: ");
+            System.out.println(respuestaStr);
+        } catch (IOException ex) {
+            System.out.println("Error inesperado al procesar la petición: " + ex.getMessage());
+        }
+    }
+    openssl s_client -connect localhost:1433  /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > sqlserver.crt
+
+
+    */
 }
